d180ad851ca86b2c7fd29e9859920e461b2bd65e
Shivaraj S <shivarajskit@gmail.com>
2025-09-11 13:16:43 +0530

    MERN Stack 30 days Practice - Express JS - Updated User Schema to include age

--- MERN-STACK/EXPRESS/users-api/models/User.js
+++ MERN-STACK/EXPRESS/users-api/models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    age: { type: Number, default: 18 },
 }, {timestamps: true}); 
 
 module.exports = mongoose.model('User', userSchema);
