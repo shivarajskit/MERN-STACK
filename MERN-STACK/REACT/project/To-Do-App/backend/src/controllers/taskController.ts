de93d1a51c99fafd713e16beb7c5914c75faf1ca
Shivaraj S <shivarajskit@gmail.com>
2025-09-19 11:06:44 +0530

    Todo app - Backend - Updating Task Model and Controller

--- MERN-STACK/REACT/project/To-Do-App/backend/src/controllers/taskController.ts
+++ MERN-STACK/REACT/project/To-Do-App/backend/src/controllers/taskController.ts
@@ -1,29 +1,44 @@
 import { Request, Response } from "express";
 import Task from "../models/Task";
 
+// Extend Express Request interface to include 'user'
+declare global {
+  namespace Express {
+    interface User {
+      id: string;
+      // add other user properties if needed
+    }
+    interface Request {
+      user: User;
+    }
+  }
+}
+
+
+
 // GET /tasks
 export const getTasks = async (req: Request, res: Response) => {
-  const tasks = await Task.find();
+  const tasks = await Task.find({ userId: req?.user.id });
   res.json(tasks);
 };
 
 // POST /tasks
 export const createTask = async (req: Request, res: Response) => {
   const { title } = req.body;
-  const task = await Task.create({ title });
+  const task = await Task.create({ title, userId: req.user.id });
   res.status(201).json(task);
 };
 
 // PATCH /tasks/:id
 export const updateTask = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const task = await Task.findByIdAndUpdate(id, req.body, { new: true });
+  const task = await Task.findByIdAndUpdate({ _id: id, userId: req.user.id}, {completed: req.body.completed }, { new: true });
   res.json(task);
 };
 
 // DELETE /tasks/:id
 export const deleteTask = async (req: Request, res: Response) => {
   const { id } = req.params;
-  await Task.findByIdAndDelete(id);
+  await Task.findByIdAndDelete({ _id: id, userId: req.user.id });
   res.json({ message: "Task deleted" });
 };
--- MERN-STACK/REACT/project/To-Do-App/backend/src/models/Task.ts
+++ MERN-STACK/REACT/project/To-Do-App/backend/src/models/Task.ts
@@ -3,12 +3,14 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface ITask extends Document {
   title: string;
   completed: boolean;
+  userId: mongoose.Schema.Types.ObjectId;
 }
 
 const TaskSchema: Schema = new Schema(
   {
     title: { type: String, required: true },
     completed: { type: Boolean, default: false },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
   },
   { timestamps: true }
 );
