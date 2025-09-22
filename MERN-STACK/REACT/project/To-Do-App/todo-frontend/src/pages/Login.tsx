0cf8d5d946f32310b1aaaca435542b70e9dc9cbf
Shivaraj S <shivarajskit@gmail.com>
2025-09-22 16:57:27 +0530

    MERN Stack Frontend - Modify Login component to use Redux and add basic form control validations

--- MERN-STACK/REACT/project/To-Do-App/todo-frontend/src/pages/Login.tsx
+++ MERN-STACK/REACT/project/To-Do-App/todo-frontend/src/pages/Login.tsx
@@ -1,21 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api";
+import { loginUser } from "../store/slices/authSlice";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
 
 const Login: React.FC = () => {
+    const dispatch = useAppDispatch();
+    const { loading, error, token } = useAppSelector((state) => state.auth);
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
+    useEffect(() => {
+        if (token) {
+            navigate('/dashboard');
+        }
+    }, [token, navigate]);
+
     const handleLogin = async (e: React.FormEvent) => {
         e.preventDefault();
-        try {
-            const response = await api.post('/api/auth/login', { email, password });
-            localStorage.setItem('token', response.data.token);
-            navigate('/dashboard');
-        }catch (error) {
-            console.error("Login failed", error);
-        }
+        // try {
+        //     const response = await api.post('/api/auth/login', { email, password });
+        //     localStorage.setItem('token', response.data.token);
+        //     navigate('/dashboard');
+        // }catch (error) {
+        //     console.error("Login failed", error);
+        // }
+        if (!email || !password) return;
+        dispatch(loginUser({ email, password }));
     }
     return (
         <div className="card">
@@ -27,6 +40,7 @@ const Login: React.FC = () => {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 className="input"
+                required
                 />
                 <input
                 type="password"
@@ -34,10 +48,16 @@ const Login: React.FC = () => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="input"
+                required
+                minLength={6}
                 />
-                <button type="submit" className="btn">
-                Login
+                <button 
+                    type="submit"
+                    disabled={loading} 
+                    className="btn">
+                {loading ? "Logging in..." : "Login"}
                 </button>
+                {error && <p className="text-red-500 mt-2">{error}</p>}
             </form>
             </div>
     );
