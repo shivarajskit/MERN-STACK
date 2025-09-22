f1555527b15dbdbf1a0db225957a224be4d2284a
Shivaraj S <shivarajskit@gmail.com>
2025-09-22 16:54:03 +0530

    MERN Stack Frontend - Wrap the App with the global store with Provider

--- MERN-STACK/REACT/project/To-Do-App/todo-frontend/src/main.tsx
+++ MERN-STACK/REACT/project/To-Do-App/todo-frontend/src/main.tsx
@@ -4,13 +4,17 @@ import App from './App.tsx';
 import './App.css';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Provider store={store }>
+        <App />
+      </Provider>
     </QueryClientProvider>
   </StrictMode>,
 )
