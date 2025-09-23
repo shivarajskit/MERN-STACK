b1a6ef266795422e2543d570e63150a23f511934
Shivaraj S <shivarajskit@gmail.com>
2025-09-23 11:56:07 +0530

    MERN Stack Frontend - Login component modification to use Redux and also form validation with YUP Schema

--- MERN-STACK/REACT/project/To-Do-App/todo-frontend/src/pages/Login.tsx
+++ MERN-STACK/REACT/project/To-Do-App/todo-frontend/src/pages/Login.tsx
@@ -3,13 +3,23 @@ import { useNavigate } from "react-router-dom";
 import api from "../api";
 import { loginUser } from "../store/slices/authSlice";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { loginSchema, type LoginSchemaType } from "../validation/loginSchema";
 
 const Login: React.FC = () => {
     const dispatch = useAppDispatch();
     const { loading, error, token } = useAppSelector((state) => state.auth);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const {
+        register, handleSubmit, formState: { errors}
+    } = useForm({
+        resolver: yupResolver(loginSchema)
+    });
+
+
+    // const [email, setEmail] = useState("");
+    // const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -18,39 +28,29 @@ const Login: React.FC = () => {
         }
     }, [token, navigate]);
 
-    const handleLogin = async (e: React.FormEvent) => {
-        e.preventDefault();
-        // try {
-        //     const response = await api.post('/api/auth/login', { email, password });
-        //     localStorage.setItem('token', response.data.token);
-        //     navigate('/dashboard');
-        // }catch (error) {
-        //     console.error("Login failed", error);
-        // }
-        if (!email || !password) return;
-        dispatch(loginUser({ email, password }));
+    const onSubmit = (data: LoginSchemaType) => {
+        dispatch(loginUser(data));
     }
     return (
         <div className="card">
             <h2 className="heading-xl">Login</h2>
-            <form onSubmit={handleLogin} className="space-y-3">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
                 <input
                 type="email"
                 placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
                 className="input"
-                required
+                {...register("email")}
                 />
+                {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
                 <input
                 type="password"
                 placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
                 className="input"
-                required
-                minLength={6}
+                {...register("password")}
                 />
+                {errors.password && (
+                    <p className="text-red-500 text-sm">{errors.password.message}</p>
+                )}
                 <button 
                     type="submit"
                     disabled={loading} 
