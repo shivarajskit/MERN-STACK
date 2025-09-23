32939a96f6b2a614a5a46c1f0862d685a2d78102
Shivaraj S <shivarajskit@gmail.com>
2025-09-23 11:56:28 +0530

    MERN Stack Frontend - Register component modification to use Redux and also form validation with YUP Schema

--- MERN-STACK/REACT/project/To-Do-App/todo-frontend/src/pages/Register.tsx
+++ MERN-STACK/REACT/project/To-Do-App/todo-frontend/src/pages/Register.tsx
@@ -1,52 +1,63 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import api from "../api";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { useForm } from "react-hook-form";
+import { registerSchema, type RegisterSchemaType } from "../validation/registerSchema";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { registerUser } from "../store/slices/authSlice";
 
 const Register: React.FC = () => {
-    const [userName, setUserName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const dispatch = useAppDispatch();
     const navigate = useNavigate();
+    const { loading, error} = useAppSelector((state) => state.auth);
 
-    const handleRegister = async (e: React.FormEvent) => {
-        e.preventDefault();
+    const { register, handleSubmit, formState: { errors} } = useForm<RegisterSchemaType>({
+        resolver: yupResolver(registerSchema)
+    })
+
+    const onSubmit = async (data: RegisterSchemaType) => {
         try {
-            await api.post('/api/auth/register', { userName, email, password });
-            alert("Registration successful! Please login.");
-            navigate('/login');
-        }catch (error) {
-            console.error("Registration failed", error);
+            await dispatch(registerUser(data)).unwrap(); // ✅ unwrap rejects on error
+            navigate("/login"); // ✅ redirect on success
+        } catch (err) {
+            console.error("Registration failed:", err);
         }
     }
     return (
         <div className="max-w-md mx-auto mt-10 p-4 border rounded card">
             <h2 className="text-xl font-bold mb-4 heading-xl">Register</h2>
-            <form onSubmit={handleRegister} className="space-y-3">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
                 <input
                 type="text"
                 placeholder="Name"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
+                {...register("userName")}
                 className="w-full p-2 border rounded input"
                 />
+                {errors.userName && <p className="text-red-500 text-sm">{errors.userName.message}</p>}
                 <input
                 type="email"
                 placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                {...register("email")}
                 className="w-full p-2 border rounded input"
                 />
+                {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
                 <input
                 type="password"
                 placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                {...register("password")}
                 className="w-full p-2 border rounded input"
                 />
-                <button type="submit" className="w-full bg-green-500 text-white p-2 rounded btn">
-                Register
+                {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
+                <button
+                    type="submit"
+                    disabled={loading}
+                    className="bg-green-600 text-white p-2 w-full rounded hover:bg-green-700"
+                >
+                    {loading ? "Registering..." : "Register"}
                 </button>
                 <Link to='/login' className="auth-link">Already have an account? Login</Link>
+                {error && <p className="text-red-600">{error}</p>}
             </form>
         </div>
     )
