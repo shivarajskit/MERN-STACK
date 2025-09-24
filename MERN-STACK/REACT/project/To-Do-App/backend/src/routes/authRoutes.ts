30705d7edccac3be9731205aed853ae6e0bdfa1c
Shivaraj S <shivarajskit@gmail.com>
2025-09-24 11:53:50 +0530

    MERN Stack Frontend - create refresh token and logout api routes

--- MERN-STACK/REACT/project/To-Do-App/backend/src/routes/authRoutes.ts
+++ MERN-STACK/REACT/project/To-Do-App/backend/src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import express = require("express");
-import { loginUser, registerUser } from "../controllers/authController";
+import { loginUser, registerUser, refreshToken, logoutUser } from "../controllers/authController";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/refresh-token", refreshToken);
+router.post("/logout", logoutUser);
 
 export default router;
