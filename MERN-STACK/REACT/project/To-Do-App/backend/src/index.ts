414adabf04d71110f8675f7092c6a590c6757af6
Shivaraj S <shivarajskit@gmail.com>
2025-09-24 11:53:09 +0530

    MERN Stack Frontend - add cookie parser and adjust cors to not throw cross origin error and also to use cookies

--- MERN-STACK/REACT/project/To-Do-App/backend/src/index.ts
+++ MERN-STACK/REACT/project/To-Do-App/backend/src/index.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db";
 import taskRoutes from "./routes/taskRoutes";
 import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 const app = express();
+app.use(cookieParser()); // enables use of cookies from the request. Eg. req.cookies.refreshToken
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173", // frontend to-do app port
+    credentials: true               // allow cookies/authorization headers
+}));
 app.use(express.json());
 
 // Routes
