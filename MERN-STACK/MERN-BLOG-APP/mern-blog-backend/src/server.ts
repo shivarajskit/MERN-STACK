852d59b6117d0f6786a1632dc902b6cd2dfbafa2
Shivaraj S <shivarajskit@gmail.com>
2025-09-30 17:31:03 +0530

    MERN BLOG APP - Backend - server setup

---
+++ MERN-STACK/MERN-BLOG-APP/mern-blog-backend/src/server.ts
@@ -0,0 +1,10 @@
+import app from './app';
+import connectDB from './config/db';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+connectDB(process.env.MONGO_URI!);
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
