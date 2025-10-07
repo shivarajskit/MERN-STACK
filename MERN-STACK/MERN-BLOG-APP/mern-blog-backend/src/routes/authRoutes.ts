ed30cfe2bec0ecfcd7203b2cbf642f02a1361c7e
Shivaraj S <shivarajskit@gmail.com>
2025-10-07 16:41:11 +0530

    MERN Stack - Blog App - Backend - Auth Routes

---
+++ MERN-STACK/MERN-BLOG-APP/mern-blog-backend/src/routes/authRoutes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { registerUser, loginUser, refreshToken, logoutUser } from "../controllers/authController";
+
+const router = Router();
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/refresh", refreshToken);
+router.post("/logout", logoutUser);
+
+export default router;
